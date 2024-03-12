a91c34399f008230eb6a9bee9b2eb7344f134749
MarcoFonti <theemporiumg@gmail.com>
2024-03-12 18:20:25 +0100

    add style and print on paje

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     public function __invoke()
     {
-        return view('train');
+        
+        $trains = Train::all();
+        
+        return view('train', compact('trains'));
     }
 }
