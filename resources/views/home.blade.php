dea3118eb853fffe67a338e2e5888e9c8792d5c3
MarcoFonti <theemporiumg@gmail.com>
2024-03-12 15:34:18 +0100

    add scaffolding

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('main-content')
+    <section>
+        <h6>MAIN</h6>
+    </section>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>TABELLA TRENI</h1>
+</header>
